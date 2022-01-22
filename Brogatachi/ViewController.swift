d4ff752ec947d0231fb0443d5bade409a60113af
Blake Branvold <37115648+Underdoneboar4@users.noreply.github.com>
2022-01-22 14:55:19 -0600

    button actions

--- Brogatachi/ViewController.swift
+++ Brogatachi/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MainImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func HomeButtomPressed(_ sender: Any) {
+    }
+    @IBAction func GymButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func FoodButtonPressed(_ sender: Any) {
+    }
+    @IBAction func LibraryButtonPressed(_ sender: Any) {
+    }
 }
 

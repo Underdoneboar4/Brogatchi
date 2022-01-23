e08119ac436a07b6df90faaca4dde94e01c2c684
Ryan Thomas <rthockey12@yahoo.com>
2022-01-22 19:50:12 -0600

    fixed formatting again

--- Brogatachi/ViewController.swift
+++ Brogatachi/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        player!.numberOfLoops = -1
         playSound(a: "song1")
         
         

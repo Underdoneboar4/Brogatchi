8dabe2ed6d67d64227d4ff1bf15e776dc3acf70f
Ryan Thomas <rthockey12@yahoo.com>
2022-01-22 22:30:44 -0600

    branch :)

---
+++ Brogatachi/GameData.swift
@@ -0,0 +1,19 @@
+//
+//  GameData.swift
+//  Brogatachi
+//
+//  Created by Ryan Thomas on 1/22/22.
+//
+
+import Foundation
+
+class GameData{
+    
+struct characterData{
+    
+    static var color = "Red"
+    
+    
+}
+    
+}

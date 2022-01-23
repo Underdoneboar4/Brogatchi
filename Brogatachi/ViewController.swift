a4b8191f13f8aabc4432f4077fc2c3f2c88daac7
Blake Branvold <37115648+Underdoneboar4@users.noreply.github.com>
2022-01-22 18:55:15 -0600

    sound

--- Brogatachi/ViewController.swift
+++ Brogatachi/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        playSound(a: "song1")
+        
+        
+        
     }
+    
+    
+    var player: AVAudioPlayer?
+
+    func playSound(a: String) {
+        guard let url = Bundle.main.url(forResource: a, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    
 
     @IBAction func HomeButtomPressed(_ sender: Any) {
         
